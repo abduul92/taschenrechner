0ae7ebe213045d71c429f6971208e5999176271d
Abdullah <abdul-1992@hotmail.de>
2025-08-25 11:50:05 +0200

    JS Funktion erstellt

--- rechner.js
+++ rechner.js
@@ -1,3 +1,20 @@
+var display = document.getElementById("display");
 
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function calculate() {
+    display.value = eval(display.value);
+}
+
+function clearDisplay(){
+    display.value = " ";
+}
+
+function clearOne(){
+    display.value = display.value.slice(0, -1);
+    
+}
 
 
